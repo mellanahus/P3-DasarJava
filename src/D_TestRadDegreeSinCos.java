fbe492c7bd3b02ca4d068b20afdc45a8c2b39d2d
mellanahus <Mellanahus02SJ@gmail.com>
2022-03-20 08:17:41 +0700

    Percobaan 4 menggunakan fungsi toDegrees(), toRadians(), sin(), dan cos()

---
+++ src/D_TestRadDegreeSinCos.java
@@ -0,0 +1,34 @@
+/* percoban menggunakan fungsi toDegrees(),
+toRadians(), sin(), dan cos()
+ */
+
+import static java.lang.Math.PI;
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
+import static java.lang.Math.toDegrees;
+import static java.lang.Math.toRadians;
+
+public class D_TestRadDegreeSinCos {
+    public static void main(String[] args) {
+
+        // value of PI in degrees
+        double rad = PI;
+        System.out.println(toDegrees(rad));
+
+        // value of PI/2 in degrees
+        rad = PI/2;
+        System.out.println(toDegrees(rad));
+
+        // value of PI/3 in degrees
+        rad = PI/3;
+        System.out.println(toDegrees(rad));
+
+        // value of PI/4 in degrees
+        rad = PI/4;
+        System.out.println(toDegrees(rad));
+
+        System.out.println("Sin 90 derajat adalah "+sin(toRadians(90)));
+        System.out.println("Cos 60 derajat adalah "+sin(toRadians(60)));
+
+    }
+}
