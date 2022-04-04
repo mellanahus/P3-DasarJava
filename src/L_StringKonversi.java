e3d3b28b407e9c636f009893ed495cbf1fb4c75b
mellanahus <Mellanahus02SJ@gmail.com>
2022-04-04 08:31:13 +0700

    Percobaan 12 mengkonversi tipe data apapun menjadi sebuah String

---
+++ src/L_StringKonversi.java
@@ -0,0 +1,14 @@
+// percobaan mengkonversi tipe data apapun menjadi sebuah String
+
+public class L_StringKonversi {
+    public static void main(String[] args) {
+
+        // konversi tipe data apapun ke String
+        double nilai = 75;
+        nilai = nilai/2;
+        System.out.println("75/2 = "+nilai);
+        boolean isOK = true;
+        System.out.println(" "+isOK);
+        System.out.println(" "+25/6.0);
+    }
+}
