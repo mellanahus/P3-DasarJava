0278e065f5f3facce484ae4d06808bc7766d4388
mellanahus <Mellanahus02SJ@gmail.com>
2022-04-04 08:37:49 +0700

    Percobaan 15 menggunakan fungsi String

---
+++ src/O_StringSub.java
@@ -0,0 +1,11 @@
+// percobaan menggunakan fungsi String
+
+public class O_StringSub {
+    public static void main(String[] args) {
+
+        String s = "Bicycle";
+        int iBegin = 1;
+        char iEnd = 3;
+        System.out.println(s.substring(iBegin,iEnd));
+    }
+}
