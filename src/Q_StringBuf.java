167929d0be49d4098f77e76c5af37ab2746fc7b9
mellanahus <Mellanahus02SJ@gmail.com>
2022-04-04 08:41:03 +0700

    Percobaan 17 menggunakan kelas StringBuffer

---
+++ src/Q_StringBuf.java
@@ -0,0 +1,14 @@
+// percobaan menggunakan kelas StringBuffer
+
+public class Q_StringBuf {
+    public static void main(String[] args) {
+
+        String str = "Java";
+        StringBuffer buffer = new StringBuffer(str);
+        if(str.equals(buffer)) {
+            System.out.println("Both are equal");
+        } else {
+            System.out.println("Both are not equal");
+        }
+    }
+}
