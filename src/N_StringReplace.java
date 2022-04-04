82a017b7758a5184f211b99591582522b26e6421
mellanahus <Mellanahus02SJ@gmail.com>
2022-04-04 08:36:04 +0700

    Percobaan 14 menggantikan nilai elemen String

---
+++ src/N_StringReplace.java
@@ -0,0 +1,10 @@
+// percobaan menggantikan nilai elemen String
+
+public class N_StringReplace {
+    public static void main(String[] args) {
+
+        String nama = "Dora";
+        nama = nama.replace("Dor","Ti");
+        System.out.println(nama);
+    }
+}
